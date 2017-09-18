bd8c8855a50da629a6187b55d452f7b7fb574bd1
Koen Hendriks <hendriks.k@ziggo.nl>
2017-09-18 19:47:27 +0200

    Lifecycle methods

---
+++ src/Components/Main.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+    constructor(){
+        super();
+    }
+
+    _log(methodName, args){
+        console.log(methodName, args);
+    }
+
+    componentWillUpdate(){
+        this._log('componentWillUpdate', arguments);
+    }
+
+    componentDidUpdate(){
+        this._log('componentDidUpdate', arguments);
+    }
+
+    componentWillMount(){
+        this._log('componentWillMount', arguments);
+    }
+
+    componentDidMount(){
+        this._log('componentDidMount', arguments);
+    }
+
+    componentWillUnmount(){
+        this._log('componentWillUnmount', arguments);
+    }
+
+    shouldComponentUpdate(newProps, newState){
+
+    }
+
+    render(){
+        return(
+            <h1>Hello World!</h1>
+        );
+    }
+}
